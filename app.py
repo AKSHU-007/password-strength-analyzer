8869c353948ae385955b18104471484d1f505f2f
Argho Kumar Halder <argho.kr07@gmail.com>
2026-05-23 18:31:11 +0530

    Fixed deployment issue

--- app.py
+++ app.py
@@ -56,8 +56,10 @@ def check():
 
 import os
 
-if __name__=="__main__":
+import os
+
+if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT",5000))
+        port=int(os.environ.get("PORT", 5000))
     )git add .
